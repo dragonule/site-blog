5d943fe9123d44995b4372d5b07a2c9253f7504d
Xiaolong <imxiaolong@users.noreply.github.com>
2019-12-25 07:34:27 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,14 +3,14 @@
 """
 
 # For Maverick
-site_prefix = "/Blog-With-GitHub-Boilerplate/"
+site_prefix = "/"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
     "enabled": False,
-    "repo": "imxiaolong/Blog-With-GitHub-Boilerplate@gh-pages"
+    "repo": "imxiaolong/imxiaolong.github.io@gh-pages"
 }
 
 # 站点设置
